e90347b2bbdeab36b8ff7c56e9208a9c40871b49
silvanodossan <silvano@superroot.eu>
2018-02-12 23:44:36 +0000

    delete state file

--- stateS3.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "lpg-terraform"
-    key    = "bob/"
-    region = "eu-west-1"
-    access_key = "AKIAJECLMIR7MVGQPSGQ"
-    secret_key = "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"
-  }
-}
