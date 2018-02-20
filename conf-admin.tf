490d2f9a8aa620b7c0a186371e76e20bd5e3ec5b
Robert M <robertm@kainos.com>
2018-02-20 15:59:17 +0000

    update to vm size and disk naming

--- conf-admin.tf
+++ conf-admin.tf
@@ -12,7 +12,7 @@ variable "admin_vm_count" {
 
 variable "admin_vm_size" {
   description = "Specifies the size of the virtual machine."
-  default     = "Standard_A1_v2"
+  default     = "Standard_A2_v2"
 }
 
 resource "azurerm_network_interface" "admin_nic" {
@@ -29,7 +29,7 @@ resource "azurerm_network_interface" "admin_nic" {
 }
 
 resource "azurerm_managed_disk" "admin_datadisk" {
-  name                 = "${var.admin_name}-appdisk-${format("%02d", count.index+1)}"
+  name                 = "${var.admin_name}-datadisk-${format("%02d", count.index+1)}"
   location             = "${lookup(var.zone, terraform.workspace)}"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
   storage_account_type = "Standard_LRS"
