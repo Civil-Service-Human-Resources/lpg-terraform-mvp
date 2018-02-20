eb3e90724f06bf33fe254bc0232565beabe38633
Robert M <robertm@kainos.com>
2018-02-20 16:03:30 +0000

    new ssh key for test of cluster code

--- variables.tf
+++ variables.tf
@@ -19,6 +19,7 @@ variable "zone" {
     "demo" = "ukwest"
     "test" = "uksouth"
     "dev" = "uksouth"
+    "testbuildcluster" = "uksouth"
   }
 }
 
@@ -72,6 +73,7 @@ variable "key_data" {
   default = {
     "test" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAp2bhqIZCvmAfFlY0j3vnWpZYqM1rf5pkK5jIUNv5MvFyWoMKtCvYz7asGYC57eJWThVKxd3O7EeRXTkku2AEEqoeMAcFy+zbo0dlWshG8X6+SJm17uvZwDOCYQsE2rqU8OtOKCRD16aj6L1eOUM5SM201S7kCuhgteIUkqYITo+9DogLFJek68tMmhiH90jKURVLFCWMDpocOjG59mAN9RmoHB9iDo4ExnV24ALhFXce7x4RiKy6Ri82Xda+wPy97o6Af3IjpzFv7uThtSMWuFU2+qJCqiOO1iekqRgWobFiesnMZUlPZ71ehKHDQ5dUyxmLtwJX+Et6iDfqpPIP superroot@MBP-SR1.local"
     "demo" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGyZ7MTiyf/1flcZJkzRJD2IRikaYyK38tWXND7nxOz6ri0WRlvxvf+HYgmuJgJOF+0tGS9Tg31+s4TzUr0uaRfMc5ITPOdpD4nbPPjDXouwNRYh+/eSf8xlMu2WrNPrFpY3s2w/2dRn43yMPz/mIqxURyDhoe3+mfAvpEjfpJbZRKO4Q44dHsZKOBf7umBWwb8BSB1H++2bpb1QBvK5lI2Dxec10wjG1u8Epay9mdXN6FNNhAjL6vhK2byQ8KTxxPinqVYmX3TGjHQnbPdpjMT+iC+fjHfyrr6hLCdoHJfP5R93X/cjaWRxEHbdcLbQdwVPYwX7C87yKo8eXKxsIx superroot@superroot.local"
+    "testbuildcluster" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQChLXffjQJbBIMsxi/SsdpfEjI5UBRna4ghRbo90FLCWe1sI6ieeODK/v8JGab2wWs2kQRQPDa73mSMZqbme6+42zlUFvIjKNXEaL2BM23z2k47mLthhiFBC7UnXntRb/8sj96HoLUjIEmZY+xIoe9IQX7pxhS4VXGKOiSKalJzNUtXqycQ56evu+J9KLE/5P3ola0RxOuqWp7LPV1rOyjf4M5N5DY2R7OJnD3cnrc4OYvfLTuvLX1mpnPRWTHSI3YtqUJdmWDsMZ7TZcTcq1ksdY2GNRjjIJ0oatCG4EStyWuq9AYYy0+w7+Q4W8MPg5zYFylTu6x+qg4BtfVctcu9 rob@rob-virtual-machine"
     "newkey" = ""
   }
 }
